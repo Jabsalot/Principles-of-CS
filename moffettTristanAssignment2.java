e31c5b31fd7c9cc12ad7788c5cba2cdf9300ec14
Tristan <tristanmoffett36@gmail.com>
2024-01-29 19:53:07 -0700

    Updated assignment #2 with more comments and slightly changed naming of certain variables

--- moffettTristanAssignment2.java
+++ moffettTristanAssignment2.java
@@ -10,21 +10,21 @@ public class moffettTristanAssignment2 {
 		String nameLoc1;
 		double windSpeedLoc1;
 		double fahrenheitLoc1;
-		double windChillLoc1F;
 		double celsiusLoc1;
-		double windChillLoc1C;
+		double windChillFahrenheitLoc1;
+		double windChillCelsiusLoc1;
 		
 		String nameLoc2;
 		double windSpeedLoc2;
 		double fahrenheitLoc2;
-		double windChillLoc2F;
 		double celsiusLoc2;
-		double windChillLoc2C;
+		double windChillFahrenheitLoc2;
+		double windChillCelsiusLoc2;
 		
 		double windSpeedDiff;
 		double fahrenheitDiff;
-		double windChillFahrenDiff;
 		double celsiusDiff;
+		double windChillFahrenheitDiff;
 		double windChillCelsiusDiff;
 		
 		// Create Scanner object that takes input from keyboard
@@ -55,50 +55,56 @@ public class moffettTristanAssignment2 {
 		// Close Scanner Object to save on memory
 		userInput.close();
 		
-		// Calculate first areas wind chill(F), Celsius, and wind chill(C)
-		windChillLoc1F = 35.74 + (0.6215 * fahrenheitLoc1) - 
-						(35.75 * Math.pow(windSpeedLoc1, EXPONENT_CONSTANT)) + 
-						(0.4275 * fahrenheitLoc1 * Math.pow(windSpeedLoc1, EXPONENT_CONSTANT));
+		// Calculate first areas wind chill(F), wind chill(C), and Celsius
+		windChillFahrenheitLoc1 = 35.74 + (0.6215 * fahrenheitLoc1) - 
+								 (35.75 * Math.pow(windSpeedLoc1, EXPONENT_CONSTANT)) + 
+								 (0.4275 * fahrenheitLoc1 * Math.pow(windSpeedLoc1, EXPONENT_CONSTANT));
+		windChillCelsiusLoc1 = (windChillFahrenheitLoc1 - 32) * (5.0/9.0);
 		celsiusLoc1 = (fahrenheitLoc1 - 32) * (5.0/9.0);
-		windChillLoc1C = (windChillLoc1F - 32) * (5.0/9.0);
 		
-		// Calculate second areas wind chill(F), Celsius, and wind chill(C)
-		windChillLoc2F = 35.74 + (0.6215 * fahrenheitLoc2) - 
-						(35.75 * Math.pow(windSpeedLoc2, EXPONENT_CONSTANT)) + 
-						(0.4275 * fahrenheitLoc2 * Math.pow(windSpeedLoc2, EXPONENT_CONSTANT));
+		// Calculate second areas wind chill(F), wind chill(C), and Celsius
+		windChillFahrenheitLoc2 = 35.74 + (0.6215 * fahrenheitLoc2) - 
+							      (35.75 * Math.pow(windSpeedLoc2, EXPONENT_CONSTANT)) + 
+							      (0.4275 * fahrenheitLoc2 * Math.pow(windSpeedLoc2, EXPONENT_CONSTANT));
+		windChillCelsiusLoc2 = (windChillFahrenheitLoc2 - 32) * (5.0/9.0);
 		celsiusLoc2 = (fahrenheitLoc2 - 32) * (5.0/9.0);
-		windChillLoc2C = (windChillLoc2F - 32) * (5.0/9.0);
 		
-		// Calculate differences between two locations
+		// Calculate differences in data between the two locations
 		windSpeedDiff = Math.abs(windSpeedLoc1 - windSpeedLoc2);
 		fahrenheitDiff = Math.abs(fahrenheitLoc1 - fahrenheitLoc2);
-		windChillFahrenDiff = Math.abs(windChillLoc1F - windChillLoc2F);
 		celsiusDiff = Math.abs(celsiusLoc1 - celsiusLoc2);
-		windChillCelsiusDiff = Math.abs(windChillLoc1C - windChillLoc2C);
+		windChillFahrenheitDiff = Math.abs(windChillFahrenheitLoc1 - windChillFahrenheitLoc2);
+		windChillCelsiusDiff = Math.abs(windChillCelsiusLoc1 - windChillCelsiusLoc2);
 		
-		// Display Data
+		// Display chart of data
 		System.out.println("*********************************************************************************************");
 		System.out.printf("%55s\n", "Wind Chill Calculator");
 		System.out.println("*********************************************************************************************\n");
 		System.out.println("Location\tWind Speed\tTemperature(F)\tWind Chill(F)\tTemperature(C)\tWind Chill(C)");
 		System.out.println("---------------------------------------------------------------------------------------------");
+		
+		// Display data related to location one
 		System.out.printf("%s\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\n", nameLoc1, 
 																		  windSpeedLoc1, 
 																		  fahrenheitLoc1, 
-																		  windChillLoc1F, 
+																		  windChillFahrenheitLoc1, 
 																		  celsiusLoc1, 
-																		  windChillLoc1C);
+																		  windChillCelsiusLoc1);
+		
+		// Display data related to location two
 		System.out.printf("%s\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\n", nameLoc2, 
 																		  windSpeedLoc2, 
 																		  fahrenheitLoc2, 
-																		  windChillLoc2F, 
+																		  windChillFahrenheitLoc2, 
 																		  celsiusLoc2, 
-																		  windChillLoc2C);
+																		  windChillCelsiusLoc2);
+		
+		// Display differences in data between both locations
 		System.out.println("---------------------------------------------------------------------------------------------");
 		System.out.printf("%s\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f\n", "Differences", 
 																	    windSpeedDiff, 
 																		fahrenheitDiff, 
-																		windChillFahrenDiff, 
+																		windChillFahrenheitDiff, 
 																		celsiusDiff, 
 																		windChillCelsiusDiff);
 
